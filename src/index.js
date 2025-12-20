50d6ea7c9085151df1c07f358f1dcd1d01e694c4
NovaShen555 <i@novashen.top>
2025-12-20 19:14:03 +0800

    Update index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { handleSchedule } from './crawler.js';
 import { renderHome, renderThread, getThreadData } from './views.js';
-import stylesCSS from './static/styles.css?raw';
-import appJS from './static/app.js?raw';
+import stylesCSS from './static/styles.css';
+import appJS from './static/app.js';
 
 export default {
   // 定时任务入口
